fa6ac96187b6fa8a26e5aade59b121af7ad524e7
thoapt <thoapham38k14@gmail.com>
2018-01-17 15:48:56 +0700

    bai8_1-10

--- cms/include/sidebar_a.php
+++ cms/include/sidebar_a.php
@@ -10,24 +10,13 @@
 	<div id="content-container">
 		<div id="section-navigation">
 			<ul class="navi">
-				<li><a href="#">Home</a>
-					<ul class="pages">
-						<li><a href="#">Home</a></li>
-						<li><a href="#">About</a></li>
-						<li><a href="#">Clients</a></li>
-						<li><a href="#">Contact Us</a></li>
-					</ul>
-				</li>
-				<li><a href="#">About</a></li>
-				<li><a href="#">Clients</a>
-					<ul class="pages">
-						<li><a href="#">Home</a></li>
-						<li><a href="#">About</a></li>
-						<li><a href="#">Clients</a></li>
-						<li><a href="#">Contact Us</a></li>
-					</ul>
-				</li>
-				<li><a href="#">Contact Us</a></li>
+				<?php
+					$q="select cat_name from categories order by position";
+					$r= mysqli_query($dbc,$q) or die("Query {$q} \n<br> MySQL error: ".mysqli_error($dbc));
+					while ($cats = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+						echo "<li><a href='index1.php'>".$cats['cat_name']."</a>";
+					}
+				?>
 			</ul>
 		</div>
 	</div>
